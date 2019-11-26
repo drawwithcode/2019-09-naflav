d44c550b7496b9ba71bb62f5b5fe7f79c4f9ca15
naflav <55958629+naflav@users.noreply.github.com>
2019-11-26 10:50:53 +0100

    Wash your fingers

--- sketch.js
+++ sketch.js
@@ -1,11 +1,42 @@
-function preload(){
-  // put preload code here
+let img;
+
+function preload() {
+  //preload images
+  soap = loadImage('/soapy.png');
+  bubble = loadImage('/bubbly.png');
 }
 
 function setup() {
-  // put setup code here
+  //canvas settings + soap placement
+  createCanvas(windowWidth, windowHeight);
+  background("#F8EBE8");
+  image(soap, 0, 0, windowWidth, windowHeight);
+
+  //text settings
+  var myText = "Please wash your fingers when entering this website.";
+  push();
+  textFont("Calistoga");
+  textAlign(CENTER);
+  textSize(10);
+  fill("#A98F8F");
+  text(myText, windowWidth / 2, 50);
+  pop();
+  fill(255, 200, 200, 20); //correct first bubble
 }
 
 function draw() {
-  // put drawing code here
+  //doing the bubbles
+
+  for (var i = 0; i < touches.length; i++) {
+
+    stroke("#FFD3CA");
+    strokeWeight(1);
+    ellipse(touches[i].x, touches[i].y, 70);
+    fill(255, 200, 200, 20);
+  }
+}
+
+function touchMoved() {
+  // prevent dragging the screen around
+  return false;
 }
